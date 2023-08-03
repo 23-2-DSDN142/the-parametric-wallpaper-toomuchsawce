210b7de5d6476b1c05fb83fe669109f6f04fc23b
toomuchsawce <140985963+toomuchsawce@users.noreply.github.com>
2023-08-03 16:46:39 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -9,7 +9,7 @@ var diamondLength = 110;
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(NINE_LANDSCAPE);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -32,13 +32,23 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
 }
 function drawDiamond(x, y, w, l) {
+  
+  fill(62, 120, 178);
   triangle((x + w/6), y, x, (y + l/3), (x + 7*w/24), (y + l/3));
+  fill(86, 154, 196);
   triangle((x + w/6), y, (x + 7*w/24), (y + l/3), (x + w/2), y);
+  fill(109, 188, 214);
   triangle((x + w/2), y, (x + 7*w/24), (y + l/3), (x + 17*w/24), (y + l/3));
+  fill(109, 188, 214);
   triangle((x + w/2), y, (x + 5*w/6), y, (x + 17*w/24), (y + l/3));
+  fill(133, 222, 232);
   triangle((x + 5*w/6), y, (x + 17*w/24), (y + l/3), (x + w), (y + l/3));
+  fill(156, 255, 250);
   triangle(x, (y + l/3), (x + 7*w/24), (y + l/3), (x + w/2), (y + l));
+  fill(62, 120, 178);
   triangle((x + 7*w/24), (y + l/3), (x + 17*w/24), (y + l/3), (x + w/2), (y + l));
+  fill(86, 154, 196);
   triangle((x + 17*w/24), (y + l/3), (x + w), (y + l/3), (x + w/2), (y + l));
+  fill(109, 188, 214);
 }
 
